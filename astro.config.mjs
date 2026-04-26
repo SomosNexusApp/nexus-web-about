f86feb5d8955a4622d542bf03c679bd6ca6e600a
José Manuel Jiménez Rodríguez <josemajimenezrodriguez8@gmail.com>
2026-04-26 22:53:49 +0200

    SEO añadido en la nueva web
    Co-Authored-By: Juan José Gamero López <jgamlop2101@g.educaand.es>
    Co-Authored-By: Rafael <203658180+rafald10@users.noreply.github.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
-export default defineConfig({}); // Final forced route refresh
+export default defineConfig({
+  site: 'https://nexus-app.es',
+  integrations: [sitemap()],
+}); // Final forced route refresh
